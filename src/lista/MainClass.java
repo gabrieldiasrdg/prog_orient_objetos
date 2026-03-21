a750f459497c08235b9bdd43e624843c7bd85a91
Gabriel Dias <astronic.biel@gmail.com>
2026-03-21 18:48:47 -0300

    fazendo a lista

---
+++ src/lista/MainClass.java
@@ -0,0 +1,23 @@
+package lista;
+
+class X
+{
+    int i = 50;
+}
+class Y
+{
+    int i = 100;
+    X x;
+}
+public class MainClass
+{
+    public static void main(String[] args)
+    {
+        X x = new X();
+        Y y = new Y();
+        y.x = x;
+        System.out.println(x.i);
+        System.out.println(y.i);
+        System.out.println(y.x.i);
+    }
+}
--- src/org/ifgoiano/poo/modelo/Main.java
+++ src/org/ifgoiano/poo/modelo/Main.java
@@ -2,17 +2,17 @@ package org.ifgoiano.poo.modelo;
 
 import java.util.Date;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         Professor professor = new Professor(1, "121314",
                 "Gabriel da Silva Vieira", "64992765422",
                 "gabriel.edu@gmail.com");
 
-        Curso curso = new Curso( "Sistemas de Informação", 1,
+        Curso curso1 = new Curso( "Sistemas de Informação", 1,
                 "Curso de tecnologia focado no mercado de trabalho");
 
-        Turma turma = new Turma("2025.1", "123454", new Date(2025,02,17), 1, Curso curso);
+        Turma turma = new Turma("2025.1", "123454", new Date(2025,02,17), 1, curso1);
+
+
     }
 }
